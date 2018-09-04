193877746e1f7ef19c0c487e275cd38af32063d8
Bharathvaj <bharathvaj1995@gmail.com>
2018-09-05 01:58:27 +0530

    remove unwanted packages

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import Editor from './Editor';
 
 class Home extends Component {
